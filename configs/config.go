867eb0a82a7f01727a571773f8df9afb3798b4df
Dev Ashar <devashar13@gmail.com>
2025-03-10 12:12:12 -0400

    feat(Congif): create function to load and read YAML config

--- configs/config.go
+++ configs/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"fmt"
 	"os"
-
 	"gopkg.in/yaml.v3"
 )
 
@@ -51,4 +50,23 @@ type Config struct {
 	} `yaml:"server"`
 }
 
+func LoadYAML(path string) (*Config,error){
+	data,err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err) 
+	}
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, fmt.Errorf("failed to parse config file: %w", err)
+	}
+	// TODO: add default port
+
+	if err := validate(&cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+
+// func validate(cfg *Config) error{
+
+// }
 }
